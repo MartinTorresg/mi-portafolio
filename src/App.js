996a431709b5da97187c8a1c8ea1064f9ba99722
MartinTorresg <mart.torresg@duocuc.cl>
2024-11-01 20:25:07 -0300

    arreglos github pages

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'; // Cambiar BrowserRouter por HashRouter
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import AboutMe from './components/AboutMe';
 import Projects from './components/Projects';
 import Resume from './components/Resume';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<AboutMe />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/resume" element={<Resume />} />
-      </Routes>
+      <div className="flex flex-col min-h-screen">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<AboutMe />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/resume" element={<Resume />} />
+        </Routes>
+        <Footer />
+      </div>
     </Router>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,11 +2,25 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-900 text-gray-500 p-4 text-center">
+    <footer className="bg-gray-900 text-gray-500 p-6 text-center mt-16">
       <p>&copy; 2024 Martin Torres. All rights reserved.</p>
-      <div className="mt-4">
-        <a href="https://github.com/MartinTorresg" className="px-2">GitHub</a>
-        <a href="https://www.linkedin.com/in/martin-torres-edc/" className="px-2">LinkedIn</a>
+      <div className="mt-4 flex justify-center space-x-4">
+        <a
+          href="https://github.com/MartinTorresg"
+          className="text-purple-400 hover:text-purple-500"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
+        <a
+          href="https://www.linkedin.com/in/martin-torres-edc/"
+          className="text-purple-400 hover:text-purple-500"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          LinkedIn
+        </a>
       </div>
     </footer>
   );
